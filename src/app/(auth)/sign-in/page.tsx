5b2c50b188eeda0115a8d5c5b0c74b4ef5f3fb90
Salman Qureshi <123854894+salman-sikku@users.noreply.github.com>
2025-06-21 11:21:05 -0400

    add user authentication

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import { SignInView } from "@/modules/auth/ui/views/Sign-in-view";
+
+export default function page() {
+  return (
+    <SignInView/>
+  )
+}
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import { SignUpView } from "@/modules/auth/ui/views/Sign-up-view";
+
+export default function page() {
+  return (
+     <SignUpView/>
+  )
+}
